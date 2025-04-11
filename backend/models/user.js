4a24ee3d9a3efe8e47ceb56bb956f13ece2decfa
SanikaPatil2004 <sanikapatil2134@gmail.com>
2025-04-11 15:30:22 +0530

    Saving local changes

--- backend/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-  fullName: String,
-  email: String,
-  password: String, // No hashing as per your request
-});
-
-module.exports = mongoose.model("User", UserSchema);
