32ffeda263c65177f669b7a6510f2ee699ff1f79
Sanika Ashok Patil <sanikapatil2134@gmail.com>
2025-04-11 19:49:30 +0530

    Update app.py

--- chatbot-deployment-main/app.py
+++ chatbot-deployment-main/app.py
@@ -24,8 +24,8 @@ def predict():
 if __name__ == "__main__":
     # app.run(debug=True)
     # app.run(port=5001, debug=True)
-  
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5001)))
+      app.run(host="0.0.0.0", port=5001)
+    #app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5001)))
 
 
 
